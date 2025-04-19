3771be4aa8580e01245d8c330746d256fc2b8af0
andre <andre@LAPTOP-IHH0EHN2>
2025-04-19 16:53:24 -0500

    Se agrega "controlador" a el login

--- src/proyectofinaledd/ProyectoFinalEDD.java
+++ src/proyectofinaledd/ProyectoFinalEDD.java
@@ -4,7 +4,7 @@ import vista.LoginVista;
 
 public class ProyectoFinalEDD {
     public static void main(String[] args) {
-        // Inicia la aplicación JavaFX
+        // Inicia la aplicación 
         LoginVista.main(args);
     }
 }
